6440e87c7dcc3e274cf0f9a60323bab13198be32
Mac <mac@Macs-MacBook-Air.local>
2023-04-23 14:54:06 +0700

    update carousel in file hero

--- components/home-page/hero/index.tsx
+++ components/home-page/hero/index.tsx
@@ -1,35 +1,23 @@
-import classes from './index.module.css';
+import classes from './index.module.scss';
 import Image from 'next/image';
-import { useState } from 'react';
-import { text } from 'stream/consumers';
-
-const maxIndexImage = 4;
+import { Carousel } from 'antd';
 
 const Hero = () => {
-  const [index, setIndex] = useState(1);
-  let imagePath = `/images/site/${index}.jpg`;
+  let array = [1, 2, 3, 4];
   return (
     <section className={classes.hero}>
       <div className={classes.contentStyle}>
-        <div className={classes.contentStyle}>
-          <Image src={imagePath} alt="An image showing owner" width={300} height={300} />
-        </div>
-
+        <Carousel autoplay>
+          {array.map((item) => {
+            let imagePath = `/images/site/${item}.jpg`;
+            return (
+              <div key={item}>
+                <Image src={imagePath} alt="An image showing owner" width={300} height={300} />
+              </div>
+            )
+          })}
+        </Carousel>
       </div>
-      <button
-
-        disabled={index === 1}
-        onClick={() => {
-          setIndex(index - 1);
-        }}>Back</button>
-      <span>{index}/{maxIndexImage}</span>
-      <button
-        disabled={index === maxIndexImage}
-        onClick={() => {
-          setIndex(index + 1);
-        }}>Next</button>
-
-
       <h1>Hi, I am Tieu Long Nu</h1>
       <p>I write blog about my life in Than Dieu Dai Hiep</p>
     </section>
